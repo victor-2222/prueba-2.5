63b17e3bc286c20e9e18fd1821b6227bee8fde4c
victor-2222 <66581146+victor-2222@users.noreply.github.com>
2020-06-24 01:08:27 -0500

    Add files via upload

---
+++ calculo_basico.java
@@ -0,0 +1,11 @@
+package calculo_basico;
+
+public class calculo_basico {
+	public static void main ( String[] args ) {
+int a=2, b=3,x;
+
+x=a*b;
+System.out.println("El producto es:"+x); 
+
+	}
+}
