aa570cb90cb4ceef1af70fba2a0f392d5acdb812
victor-2222 <66581146+victor-2222@users.noreply.github.com>
2020-06-24 10:11:57 -0500

    actualizar

--- prueba/src/prueba/Calculo_basico.java
+++ prueba/src/prueba/Calculo_basico.java
@@ -2,9 +2,10 @@ package prueba;
 
 public class Calculo_basico {
 	public static void main ( String[] args ) {
-int a=2, b=3,x;
+int a=2, b=3,x,g=2;
 
-x=a*b;
+x=a*b*g;
+
 System.out.println("El producto es:"+x); 
 
 	}
