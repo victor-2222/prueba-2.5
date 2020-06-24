eed061855d5dfeb10e02834027cf9818da4bde79
victor-2222 <66581146+victor-2222@users.noreply.github.com>
2020-06-24 10:37:12 -0500

    actualizacion de codigo

--- prueba 3/src/prueba/calculo.java
+++ prueba 3/src/prueba/calculo.java
@@ -2,9 +2,9 @@ package prueba;
 
 public class calculo {
 	public static void main ( String[] args ) {
-int a=2, b=3,x,g=2;
+int a=2, b=3,x,s=2;
 
-x=a*b*g;
+x=a*b*s;
 
 System.out.println("El producto es:"+x); 
 
